1dbb36c1e425578556e7a209bc491563e1497d59
mendesx3 <mendesx3@gmail.com>
2023-01-13 15:55:17 -0400

    implementação para monitorar hastag ou user

--- app/main.go
+++ app/main.go
@@ -12,23 +12,35 @@ func main() {
 
 	interval := 5 * time.Second
 	clearInterval := 2 * time.Hour
-	query := "#crypto #BTC"
+	query := "#crypto"
 	scraper := twitterscraper.New()
 
-	for range time.Tick(interval) {
-		for tweet := range scraper.SearchTweets(context.Background(), query, 50) {
-			if tweet.Error != nil {
-				panic(tweet.Error)
-			}
-			if seen[tweet.ID] {
-				continue
-			}
-			seen[tweet.ID] = true
-			fmt.Println(tweet.Text)
-		}
+	ticker := time.NewTicker(interval)
+	clearTicker := time.NewTicker(clearInterval)
 
-		for range time.Tick(clearInterval) {
-			seen = make(map[string]bool)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				for tweet := range scraper.SearchTweets(context.Background(), query, 50) {
+					if tweet.Error != nil {
+						fmt.Println(tweet.Error)
+						continue
+					}
+					if seen[tweet.ID] {
+						continue
+					}
+					seen[tweet.ID] = true
+					fmt.Println(tweet.Text)
+				}
+			case <-clearTicker.C:
+				fmt.Println("clearing seen map")
+				seen = make(map[string]bool)
+			}
 		}
-	}
+	}()
+
+	<-time.After(10 * time.Minute)
+	ticker.Stop()
+	clearTicker.Stop()
 }
