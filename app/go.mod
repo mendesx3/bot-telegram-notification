e905f147c19f5be15554e3c8176c41a786474d0e
mendesx3 <mendesx3@gmail.com>
2023-01-12 23:15:46 -0400

    implementação para monitorar hastag ou user

--- app/go.mod
+++ app/go.mod
@@ -3,6 +3,16 @@ module github.com/mendesx3/bot-telegram-notification
 go 1.19
 
 require (
-	github.com/n0madic/twitter-scraper v0.0.0-20230110113340-70280ccfb6c5 // indirect
+	github.com/ChimeraCoder/anaconda v2.0.0+incompatible
+	golang.org/x/oauth2 v0.4.0
+)
+
+require (
+	github.com/ChimeraCoder/tokenbucket v0.0.0-20131201223612-c5a927568de7 // indirect
+	github.com/azr/backoff v0.0.0-20160115115103-53511d3c7330 // indirect
+	github.com/dustin/go-jsonpointer v0.0.0-20160814072949-ba0abeacc3dc // indirect
+	github.com/dustin/gojson v0.0.0-20160307161227-2e71ec9dd5ad // indirect
+	github.com/garyburd/go-oauth v0.0.0-20180319155456-bca2e7f09a17 // indirect
+	github.com/n0madic/twitter-scraper v0.0.0-20230112093343-9231dc0708ee // indirect
 	golang.org/x/net v0.5.0 // indirect
 )
--- app/main.go
+++ app/main.go
@@ -4,66 +4,26 @@ import (
 	"context"
 	"fmt"
 	twitterscraper "github.com/n0madic/twitter-scraper"
+	"time"
 )
 
 func main() {
-	func1()
-	func2()
-	func3()
-	func4()
+	var seen = make(map[string]bool)
 
-}
-
-func func1() {
+	interval := 5 * time.Second
+	query := "#crypto"
 	scraper := twitterscraper.New()
 
-	for tweet := range scraper.GetTweets(context.Background(), "", 10) {
-		if tweet.Error != nil {
-			panic(tweet.Error)
+	for range time.Tick(interval) {
+		for tweet := range scraper.SearchTweets(context.Background(), query, 100) {
+			if tweet.Error != nil {
+				panic(tweet.Error)
+			}
+			if seen[tweet.ID] {
+				continue
+			}
+			seen[tweet.ID] = true
+			fmt.Println(tweet.Text)
 		}
-		fmt.Println(tweet.Text)
 	}
 }
-
-func func2() {
-	scraper := twitterscraper.New()
-	tweet, err := scraper.GetTweet("")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(tweet.Text)
-}
-
-func func3() {
-	scraper := twitterscraper.New()
-
-	for tweet := range scraper.SearchTweets(context.Background(),
-		"#crypto", 50) {
-		if tweet.Error != nil {
-			panic(tweet.Error)
-		}
-		fmt.Println(tweet.Text)
-	}
-}
-
-func func4() {
-	scraper := twitterscraper.New()
-	trends, err := scraper.GetTrends()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(trends)
-}
-
-//func metodo5() {
-//
-//	// search tweets with hashtag golang
-//	tweets, err := twitter_scraper.ScrapeSearch("#golang", 100)
-//	if err != nil {
-//		fmt.Println(err)
-//		return
-//	}
-//	for _, tweet := range tweets {
-//		fmt.Println(tweet.User.Username, ": ", tweet.Text)
-//	}
-//}
